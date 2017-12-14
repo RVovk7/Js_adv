f92a758c2fa487c5ce38860ddbf529e01cd3d4de
RVovk7 <RVovk7@gmail.com>
2017-12-14 02:15:01 +0200

    1.02 bug fix

---
+++ css/X_O.js
@@ -0,0 +1,13 @@
+var game = ['-','-','-','-','-','-','-','-','-',];
+
+
+var currentPlayer  = playerX;
+
+function btnClick(event,arrid) {
+	if game[arrId] === '-'){
+event.target.value = curentPlayer;
+}
+game[arrId] = currentPlayer;
+switchPlayer();
+	showGameStatus();
+	setPlayerStatusElementText("player' " +currentPlayer + "'");
--- js/t2.js
+++ js/t2.js
@@ -1,37 +1,58 @@
 'use strict';
+let h_null = String.fromCharCode(160); // charCode(160) == &nbsp; in html
  inp.addEventListener('click', setOX);
+  let boadr_fix;
   let count = 0;
    function setOX() {
-	   funck[count = (count % 2) +1]();
+	   funck[count = (count % 2) +1](); // (count % 2) +1  == 1 || 2
   arpush();
-}	
+ }	
 let funck = {
 1: function setX() {
+	if ( event.target.value == h_null ){ 
 event.target.value = 'X';
 event.target.style.color = 'blue';
+}
+else { 
+	count = count  +1;    // double click fix
+	event.target.style.borderColor = 'red';
+	boadr_fix = event.target.id;
+ setTimeout(board, 500);
+	 } 
 },
 2: function setO() {
- event.target.value = 'O';
+	if (event.target.value == h_null ){
+ event.target.value = 'O'; 
  event.target.style.color = 'red';
  }
-};
+ else {
+	  count = count  +1;
+	event.target.style.borderColor = 'red';
+	boadr_fix = event.target.id;
+setTimeout(board, 500);
+ }
+  }
+ };
+function board(){
+	document.getElementById(boadr_fix).style.borderColor = 'white';
+}
 function arpush (){
- 	 let arr= {};
+ 	 let obj= {};
 	 for (let i = 1; i<=9; i++){
-	arr[document.getElementById(i).id] = document.getElementById(i).value;
+	obj[document.getElementById(i).id] = document.getElementById(i).value;  // id of button [1-9]
 }
-whowin(arr);
+whowin(obj);
 }
-function whowin(arr){
+function whowin(obj){
 	let tr = [['1','2','3'],['1','4','7'],['7','8','9'],['3','6','9'],['2','5','8'],['1','5','9'],['3','5','7'],['4','5','6']];
 	let tf_O;
  	let tf_X;
  	tr.forEach(function(el){
-		if ( arr[el[0]] == 'O' && arr[el[1]] == 'O' && arr[el[2]] == 'O' ){
+		if ( obj[el[0]] == 'O' && obj[el[1]] == 'O' && obj[el[2]] == 'O' ){
 		tf_O = true;
 		}});	
  	tr.forEach(function(el){
-		if ( arr[el[0]] == 'X' && arr[el[1]] == 'X' && arr[el[2]] == 'X' ){
+		if ( obj[el[0]] == 'X' && obj[el[1]] == 'X' && obj[el[2]] == 'X' ){
 		tf_X = true;
 		}});
 		if (tf_O){
@@ -47,13 +68,20 @@ function whowin(arr){
 	else{
 	let dro = ['1','2','3','4','5','6','7','8','9'];
 let draw = dro.every(function(el){
-		return arr[el] !== String.fromCharCode(160);
+		return obj[el] !== h_null;    
 		});
 		if (draw & !tf_X && !tf_O ){
 			document.getElementById('resault').value = 'A draw';
 			document.getElementById('restart').style.display ="block";
 		}}}
+		
 restart.addEventListener('click', restr);
 function restr(){
-	location.reload();
+	for (let i = 1; i<=9; i++){
+	document.getElementById(i).value = h_null;
+	}
+document.getElementById('resault').value = h_null;
+document.getElementById('restart').style.display ="none";
+document.getElementById('resault').style.color = 'silver';
+count = 0;
 }
